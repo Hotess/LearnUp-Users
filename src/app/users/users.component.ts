a7df8a418e12215a5652aeada5573f433558adba
Pavel Opolovnin <hote_95@bk.ru>
2022-05-05 19:54:48 +0300

    fix: scroll

--- src/app/users/users.component.ts
+++ src/app/users/users.component.ts
@@ -8,22 +8,55 @@ import {Users} from '../users';
     styleUrls: ['./users.component.scss']
 })
 export class UsersComponent implements OnInit {
-   public users: Users[] = [];
+    public users: Users[] = [];
+    public userCounter: number = 35;
+    public currentCount: number = 0;
+    private SCROLL_LOADING_GAP: number = 100;
 
     constructor(private usersService: UsersService) {}
 
-    public getUsers(): void {
+    public getAllUsers() {
         this.usersService.getUsers()
-            .subscribe(users => this.users = users);
+            .subscribe(users => {
+                this.users = users.slice(this.currentCount, this.userCounter)
+
+            });
     }
 
     public deleteUser(user: Users): void {
         this.users = this.users.filter(us => us != user);
         this.usersService.deleteUser(user)
-            .subscribe();
+            .subscribe(() => {
+                this.SCROLL_LOADING_GAP--;
+            });
     }
 
     ngOnInit(): void {
-        this.getUsers();
+        this.getAllUsers();
+    }
+
+    ngAfterViewInit(): void {
+        document.addEventListener("scroll", () => {
+            this.onScroll();
+        });
+
+        this.loadNewData();
+    }
+
+    public onScroll() {
+        if (this.isCameraTouchesBottom()) {
+            this.loadNewData();
+        }
+    }
+
+    public loadNewData(): void {
+        if (this.userCounter >= this.SCROLL_LOADING_GAP) return;
+        this.userCounter++;
+
+        this.getAllUsers();
+    }
+
+    private isCameraTouchesBottom(): boolean {
+        return (document.body.offsetHeight + document.body.offsetTop <= window.scrollY + window.innerHeight + this.SCROLL_LOADING_GAP);
     }
 }
